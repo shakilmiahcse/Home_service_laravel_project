a2a821c2a3669250002d53858e616fe3bc384eb5
Shakil Miah <shakilmiahcse36@gmail.com>
2023-08-15 10:55:39 +0600

    Create Service Details Page

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Livewire\Admin\AdminServicesByCategoryComponent;
 use App\Http\Livewire\Customer\CustomerDashboardComponent;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ServiceCategoriesComponent;
+use App\Http\Livewire\ServiceDetailsComponent;
 use App\Http\Livewire\ServicesByCategoryComponent;
 use App\Http\Livewire\Sprovider\SproviderDashboardComponent;
 use Illuminate\Support\Facades\Route;
@@ -33,6 +34,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/',HomeComponent::class)->name('home');
 Route::get('/service-categories',ServiceCategoriesComponent::class)->name('home.service_categories');
 Route::get('/{category_slug}/services',ServicesByCategoryComponent::class)->name('home.services_by_category');
+Route::get('/service/{service_slug}',ServiceDetailsComponent::class)->name('home.service_details');
 
 //for customer
 Route::middleware(['auth:sanctum','verified'])->group(function(){
