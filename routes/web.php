fc2b040adfbc831e7c33e3ed3cbf74db4de7e43f
Inzamamul Haque <inzamamul27101999@gmail.com>
2023-08-17 01:10:05 +0600

    Create Service Provider's Profile

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use App\Http\Livewire\ServiceCategoriesComponent;
 use App\Http\Livewire\ServiceDetailsComponent;
 use App\Http\Livewire\ServicesByCategoryComponent;
 use App\Http\Livewire\Sprovider\SproviderDashboardComponent;
+use App\Http\Livewire\Sprovider\SproviderProfileComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -51,6 +52,7 @@ Route::middleware(['auth:sanctum','verified'])->group(function(){
 //for service provider
 Route::middleware(['auth:sanctum','verified','authsprovider'])->group(function(){
     Route::get('/sprovider/dashboard',SproviderDashboardComponent::class)->name('sprovider.dashboard');
+    Route::get('/sprovider/profile',SproviderProfileComponent::class)->name('sprovider.profile');
 });
 
 //for admin
