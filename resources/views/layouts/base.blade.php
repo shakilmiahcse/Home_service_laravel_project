ded90a0b30220cf7114c8f38df7623fe719527f9
Inzamamul Haque <inzamamul27101999@gmail.com>
2023-08-16 13:24:12 +0600

    Making Slider Dynamic add and But there is an Error on Edit Slider

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -128,6 +128,7 @@
                                     <li><a href="{{route('admin.dashboard')}}">Dashboard</a></li>
                                     <li><a href="{{route('admin.service_categories')}}">Service Categories</a></li>
                                     <li><a href="{{route('admin.all_services')}}">All Service</a></li>
+                                    <li><a href="{{route('admin.slider')}}">Manage Slider</a></li>
                                     <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a></li>
                                 </ul>
                             </li>
--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,13 @@ use App\Http\Livewire\Admin\AdminDashboardComponent;
 use App\Http\Livewire\Admin\AdminServiceCategoryComponent;
 use App\Http\Livewire\Admin\AdminAddServiceCategoryComponent;
 use App\Http\Livewire\Admin\AdminAddServiceComponent;
+use App\Http\Livewire\Admin\AdminAddSlideComponent;
 use App\Http\Livewire\Admin\AdminEditServiceCategoryComponent;
 use App\Http\Livewire\Admin\AdminEditServiceComponent;
+use App\Http\Livewire\Admin\AdminEditSlideComponent;
 use App\Http\Livewire\Admin\AdminServicesComponent;
 use App\Http\Livewire\Admin\AdminServicesByCategoryComponent;
+use App\Http\Livewire\Admin\AdminSliderComponent;
 use App\Http\Livewire\Customer\CustomerDashboardComponent;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ServiceCategoriesComponent;
@@ -56,6 +59,10 @@ Route::middleware(['auth:sanctum','verified','authadmin'])->group(function(){
     Route::get('/admin{category_slug}/services',AdminServicesByCategoryComponent::class)->name('admin.services_by_category');
     Route::get('/admin/service/add',AdminAddServiceComponent::class)->name('admin.add_service');
     Route::get('/admin/service/edit/{service_slug}',AdminEditServiceComponent::class)->name('admin.edit_service');
+
+    Route::get('/admin/slider',AdminSliderComponent::class)->name('admin.slider');
+    Route::get('/admin/slide/add',AdminAddSlideComponent::class)->name('admin.add_slide');
+    Route::get('/admin/slide/edit/{slide_id}',AdminEditSlideComponent::class)->name('admin.edit_slide');
 });
 
 
